c2c65cdde212ed1748c7a7b61fb8731c1cc3ed9b
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-10-20 20:22:50 +0300

    Add files via upload

--- src/main/resources/db/migration/V20211018120832__init.sql
+++ src/main/resources/db/migration/V20211018120832__init.sql
@@ -1,30 +1,25 @@
 CREATE TABLE IF NOT EXISTS 'user'
 (
-	'id'		BIGINT		NOT NULL AUTO_INCREMENT,
-	'role_id'	BIGINT		NOT NULL,
+	'id'		BIGINT			NOT NULL AUTO_INCREMENT,
 	'login'		VARCHAR(32)  	NOT NULL,
-	'password'	VARCHAR(256) 	NOT NULL,
+	'password'	VARCHAR(256)	NOT NULL,
 	'name'		VARCHAR(32)  	NOT NULL,
-	'phone'		BIGINT 		NULL,
+	'phone'		BIGINT 			NULL,
 	'email'		VARCHAR(32)  	NULL,
 	PRIMARY KEY ('id'),
 	CONSTRAINT UNIQUE_LOGIN UNIQUE ('login')
-		FOREIGN KEY ('role_id')
-			REFERENCES 'role' ('id')
-			ON DELETE NO ACTION
-			ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS 'user_role_link' (
 	'role_id'	BIGINT	 NOT NULL,
 	'user_id'	BIGINT	 NOT NULL,
 	PRIMARY KEY ('role_id', 'user_id'),
-    CONSTRAINT 'user_role_link_user'
+    CONSTRAINT 'user_role_link_user_fk'
     	FOREIGN KEY ('user_id')
         	REFERENCES 'user' ('id')
             	ON DELETE NO ACTION
             	ON UPDATE NO ACTION,
-    CONSTRAINT 'user_role_link_role'
+    CONSTRAINT 'user_role_link_role_fk'
         FOREIGN KEY ('role_id')
             REFERENCES 'role' ('id')
             ON DELETE NO ACTION
@@ -39,29 +34,28 @@ CREATE TABLE IF NOT EXISTS 'role' (
 );
 
 CREATE TABLE IF NOT EXISTS 'board_game' (
-	'id'		BIGINT		NOT NULL AUTO_INCREMENT,
-	'name'		VARCHAR(512)	NOT NULL,
-	'price'		BIGINT 		NOT NULL,
-	'rent_price'	BIGINT 		NOT NULL,
-	'qoantity'	BIGINT 		NULL,
+	'id'			BIGINT			NOT NULL AUTO_INCREMENT,
+	'name'			VARCHAR(512)	NOT NULL,
+	'rent_price'	BIGINT 			NOT NULL,
+	'qoantity'		BIGINT 			NULL,
 	PRIMARY KEY ('id'),
 	CONSTRAINT UNIQUE_NAME UNIQUE ('name')
 );
 
 CREATE TABLE IF NOT EXISTS 'rent' (
-	'id'		BIGINT	NOT NULL AUTO_INCREMENT,
+	'id'			BIGINT	NOT NULL AUTO_INCREMENT,
 	'board_game_id'	BIGINT	NOT NULL,
-	'order_id'	BIGINT	NOT NULL,
-	'from'		DATE	NOT NULL,
-	'to'		DATE	NOT NULL,
-	'price'		BIGINT 	NOT NULL,
+	'order_id'		BIGINT	NOT NULL,
+	'from'			DATE	NOT NULL,
+	'to'			DATE	NOT NULL,
+	'price'			BIGINT 	NOT NULL,
 	PRIMARY KEY ('id'),
-    CONSTRAINT 'rent_board_game'
+    CONSTRAINT 'rent_board_game_fk'
         FOREIGN KEY ('board_game_id')
             REFERENCES 'board_game' ('id')
             ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    CONSTRAINT 'rent_order'
+    CONSTRAINT 'rent_order_fk'
         FOREIGN KEY ('order_id')
             REFERENCES 'order' ('id')
             ON DELETE NO ACTION
@@ -69,13 +63,13 @@ CREATE TABLE IF NOT EXISTS 'rent' (
 );
 
 CREATE TABLE IF NOT EXISTS 'order' (
-	'id'		BIGINT		NOT NULL AUTO_INCREMENT,
-	'user_id'	BIGINT		NOT NULL,
+	'id'			BIGINT		NOT NULL AUTO_INCREMENT,
+	'user_id'		BIGINT		NOT NULL,
 	'total_price'	BIGINT 		NOT NULL,
-	'date'		DATE		NOT NULL,
-	'status'	VARCHAR(32)	NOT NULL,
+	'date'			DATE		NOT NULL,
+	'status'		VARCHAR(32)	NOT NULL,
 	PRIMARY KEY ('id'),
-    CONSTRAINT 'order_user'
+    CONSTRAINT 'order_user_fk'
         FOREIGN KEY ('user_id')
             REFERENCES 'user' ('id')
             ON DELETE NO ACTION
@@ -83,29 +77,31 @@ CREATE TABLE IF NOT EXISTS 'order' (
 );
 
 CREATE TABLE IF NOT EXISTS 'accessory' (
-	'id'		BIGINT		NOT NULL AUTO_INCREMENT,
+	'id'		BIGINT			NOT NULL AUTO_INCREMENT,
 	'name'		VARCHAR(512) 	NOT NULL,
-	'price'		BIGINT 		NOT NULL,
-	'qoantity'	BIGINT 		NULL,
+	'price'		BIGINT 			NOT NULL,
+	'quantity'	BIGINT 			NULL,
 	PRIMARY KEY ('id'),
 	CONSTRAINT UNIQUE_NAME UNIQUE ('name')
 );
 
 CREATE TABLE IF NOT EXISTS 'purchase' (
-	'id'		BIGINT	NOT NULL AUTO_INCREMENT,
+	'id'			BIGINT	NOT NULL AUTO_INCREMENT,
 	'accessory_id'	BIGINT	NOT NULL,
-	'order_id'	BIGINT	NOT NULL,
-	'quantity'	BIGINT	NOT NULL,
-	'price'		BIGINT 	NOT NULL,
+	'order_id'		BIGINT	NOT NULL,
+	'quantity'		BIGINT	NOT NULL,
+	'price'			BIGINT 	NOT NULL,
 	PRIMARY KEY ('id'),
-    CONSTRAINT 'purchase_accessory'
+    CONSTRAINT 'purchase_accessory_fk'
         FOREIGN KEY ('accessory_id')
             REFERENCES 'accessory' ('id')
             ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    CONSTRAINT 'purchase_order'
+    CONSTRAINT 'purchase_order_fk'
         FOREIGN KEY ('order_id')
             REFERENCES 'order' ('id')
             ON DELETE NO ACTION
             ON UPDATE NO ACTION
 );
+
+COMMIT;
