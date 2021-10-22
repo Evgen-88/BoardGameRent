3a17b04f47ee06055cd09e8dec01c97b1a90cd9a
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-10-22 08:47:36 +0300

    Update V20211018120832__init.sql

--- src/main/resources/db/migration/V20211018120832__init.sql
+++ src/main/resources/db/migration/V20211018120832__init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS 'user'
 (
-	'id'		BIGINT			NOT NULL AUTO_INCREMENT,
+	'id'		BIGINT		NOT NULL AUTO_INCREMENT,
 	'login'		VARCHAR(32)  	NOT NULL,
 	'password'	VARCHAR(256)	NOT NULL,
 	'name'		VARCHAR(32)  	NOT NULL,
-	'phone'		BIGINT 			NULL,
+	'phone'		BIGINT 		NULL,
 	'email'		VARCHAR(32)  	NULL,
 	PRIMARY KEY ('id'),
 	CONSTRAINT UNIQUE_LOGIN UNIQUE ('login')
@@ -34,21 +34,21 @@ CREATE TABLE IF NOT EXISTS 'role' (
 );
 
 CREATE TABLE IF NOT EXISTS 'board_game' (
-	'id'			BIGINT			NOT NULL AUTO_INCREMENT,
-	'name'			VARCHAR(512)	NOT NULL,
-	'rent_price'	BIGINT 			NOT NULL,
-	'qoantity'		BIGINT 			NULL,
+	'id'		BIGINT		NOT NULL AUTO_INCREMENT,
+	'name'		VARCHAR(512)	NOT NULL,
+	'rent_price'	BIGINT 		NOT NULL,
+	'qoantity'	BIGINT 		NULL,
 	PRIMARY KEY ('id'),
 	CONSTRAINT UNIQUE_NAME UNIQUE ('name')
 );
 
 CREATE TABLE IF NOT EXISTS 'rent' (
-	'id'			BIGINT	NOT NULL AUTO_INCREMENT,
+	'id'		BIGINT	NOT NULL AUTO_INCREMENT,
 	'board_game_id'	BIGINT	NOT NULL,
-	'order_id'		BIGINT	NOT NULL,
-	'from'			DATE	NOT NULL,
-	'to'			DATE	NOT NULL,
-	'price'			BIGINT 	NOT NULL,
+	'order_id'	BIGINT	NOT NULL,
+	'from'		DATE	NOT NULL,
+	'to'		DATE	NOT NULL,
+	'price'		BIGINT 	NOT NULL,
 	PRIMARY KEY ('id'),
     CONSTRAINT 'rent_board_game_fk'
         FOREIGN KEY ('board_game_id')
@@ -59,11 +59,11 @@ CREATE TABLE IF NOT EXISTS 'rent' (
 );
 
 CREATE TABLE IF NOT EXISTS 'order' (
-	'id'			BIGINT		NOT NULL AUTO_INCREMENT,
-	'user_id'		BIGINT		NOT NULL,
+	'id'		BIGINT		NOT NULL AUTO_INCREMENT,
+	'user_id'	BIGINT		NOT NULL,
 	'total_price'	BIGINT 		NOT NULL,
-	'date'			DATE		NOT NULL,
-	'status'		VARCHAR(32)	NOT NULL,
+	'date'		DATE		NOT NULL,
+	'status'	VARCHAR(32)	NOT NULL,
 	PRIMARY KEY ('id'),
     CONSTRAINT 'order_user_fk'
         FOREIGN KEY ('user_id')
@@ -71,20 +71,20 @@ CREATE TABLE IF NOT EXISTS 'order' (
 );
 
 CREATE TABLE IF NOT EXISTS 'accessory' (
-	'id'		BIGINT			NOT NULL AUTO_INCREMENT,
+	'id'		BIGINT		NOT NULL AUTO_INCREMENT,
 	'name'		VARCHAR(512) 	NOT NULL,
-	'price'		BIGINT 			NOT NULL,
-	'quantity'	BIGINT 			NULL,
+	'price'		BIGINT 		NOT NULL,
+	'quantity'	BIGINT 		NULL,
 	PRIMARY KEY ('id'),
 	CONSTRAINT UNIQUE_NAME UNIQUE ('name')
 );
 
 CREATE TABLE IF NOT EXISTS 'purchase' (
-	'id'			BIGINT	NOT NULL AUTO_INCREMENT,
+	'id'		BIGINT	NOT NULL AUTO_INCREMENT,
 	'accessory_id'	BIGINT	NOT NULL,
-	'order_id'		BIGINT	NOT NULL,
-	'quantity'		BIGINT	NOT NULL,
-	'price'			BIGINT 	NOT NULL,
+	'order_id'	BIGINT	NOT NULL,
+	'quantity'	BIGINT	NOT NULL,
+	'price'		BIGINT 	NOT NULL,
 	PRIMARY KEY ('id'),
     CONSTRAINT 'purchase_accessory_fk'
         FOREIGN KEY ('accessory_id')
