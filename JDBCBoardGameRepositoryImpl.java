1fd2ba666345a51542e66fd694febb2307fff913
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-10-25 08:59:10 +0300

    Add files via upload

---
+++ JDBCBoardGameRepositoryImpl.java
@@ -0,0 +1,154 @@
+package com.itrex.konoplyanik.boardgamerent.repository.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import com.itrex.konoplyanik.boardgamerent.entity.BoardGame;
+import com.itrex.konoplyanik.boardgamerent.exception.RepositoryException;
+import com.itrex.konoplyanik.boardgamerent.repository.BoardGameRepository;
+
+public class JDBCBoardGameRepositoryImpl implements BoardGameRepository {
+	private static final String ID_COLUMN = "id";
+	private static final String NAME_COLUMN = "name";
+	private static final String RENT_PRICE_COLUMN = "rent_price";
+	private static final String QUANTITY_COLUMN = "quantity";
+
+	private static final String INSERT_BOARD_GAME_QUERY = "INSERT INTO board_game(name, rent_price, quantity) VALUES (?, ?, ?)";
+	private static final String SELECT_ALL_QUERY = "SELECT	* FROM board_game";
+	private static final String SELECT_BY_ID_QUERY = "SELECT * FROM board_game WHERE id=?";
+	private static final String UPDATE_BOARD_GAME_QUERY = "UPDATE board_game SET name=?, rent_price=?, quantity=? WHERE id=?";
+	private static final String DELETE_BOARD_GAME_QUERY = "DELETE FROM board_game WHERE id=?";
+
+	private final DataSource dataSource;
+
+	public JDBCBoardGameRepositoryImpl(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	@Override
+	public List<BoardGame> findAll() throws RepositoryException {
+		List<BoardGame> boardGames = new ArrayList<>();
+		try (Connection con = dataSource.getConnection();
+				Statement stm = con.createStatement();
+				ResultSet resultSet = stm.executeQuery(SELECT_ALL_QUERY)) {
+			while (resultSet.next()) {
+				BoardGame boardGame = new BoardGame();
+				boardGame = getBoardGame(resultSet, boardGame);
+				boardGames.add(boardGame);
+			}
+		} catch (SQLException ex) {
+			throw new RepositoryException("EXCEPTION: findAll: " + ex);
+		}
+		return boardGames;
+	}
+
+	@Override
+	public BoardGame findById(Long id) throws RepositoryException {
+		BoardGame boardGame = new BoardGame();
+		try (Connection con = dataSource.getConnection();
+				Statement stm = con.createStatement();
+				ResultSet resultSet = stm.executeQuery(SELECT_BY_ID_QUERY + id)) {
+			if (resultSet.next()) {
+				boardGame = getBoardGame(resultSet, boardGame);
+			}
+		} catch (SQLException ex) {
+			throw new RepositoryException("EXCEPTION: findById: " + ex);
+		}
+		return boardGame;
+	}
+
+	@Override
+	public List<BoardGame> addAll(List<BoardGame> boardGames) throws RepositoryException {
+		try (Connection con = dataSource.getConnection()) {
+			con.setAutoCommit(false);
+			try {
+				for (BoardGame bg : boardGames) {
+					insertBoardGame(con, bg);
+				}
+				con.commit();
+			} catch (SQLException ex) {
+				con.rollback();
+				throw new RepositoryException("EXCEPTION: insert boardGames: " + ex);
+			} finally {
+				con.setAutoCommit(true);
+			}
+		} catch (SQLException ex) {
+			throw new RepositoryException("EXCEPTION: addAll: " + ex);
+		}
+		return boardGames;
+	}
+
+	@Override
+	public BoardGame add(BoardGame boardGame) throws RepositoryException {
+		try (Connection con = dataSource.getConnection()) {
+			insertBoardGame(con, boardGame);
+		} catch (SQLException ex) {
+			throw new RepositoryException("EXCEPTION: add: " + ex);
+		}
+		return boardGame;
+	}
+
+	@Override
+	public BoardGame update(BoardGame boardGame) throws RepositoryException {
+		try (Connection con = dataSource.getConnection();
+				PreparedStatement preparedStatement = con.prepareStatement(UPDATE_BOARD_GAME_QUERY,
+						Statement.RETURN_GENERATED_KEYS)) {
+			preparedStatement.setString(1, boardGame.getName());
+			preparedStatement.setInt(2, boardGame.getRentPrice());
+			preparedStatement.setInt(3, boardGame.getQuantity());
+			preparedStatement.setLong(4, boardGame.getId());
+
+			if (preparedStatement.executeUpdate() > 0) {
+				return boardGame;
+			}
+		} catch (SQLException ex) {
+			throw new RepositoryException("EXCEPTION: update: " + ex);
+		}
+		return null;
+	}
+
+	@Override
+	public boolean delete(Long id) throws RepositoryException {
+		try(Connection con = dataSource.getConnection();PreparedStatement preparedStatement = con.prepareStatement(DELETE_BOARD_GAME_QUERY)) {
+			preparedStatement.setLong(1, id);
+			return preparedStatement.executeUpdate() == 1;
+		} catch(SQLException ex) {
+			throw new RepositoryException("Exception: delete: " + ex);
+		}
+	}
+
+	private BoardGame getBoardGame(ResultSet resultSet, BoardGame boardGame) throws SQLException {
+		boardGame.setId(resultSet.getLong(ID_COLUMN));
+		boardGame.setName(resultSet.getString(NAME_COLUMN));
+		boardGame.setRentPrice(resultSet.getInt(RENT_PRICE_COLUMN));
+		boardGame.setQuantity(resultSet.getInt(QUANTITY_COLUMN));
+		return boardGame;
+	}
+
+	private void insertBoardGame(Connection con, BoardGame boardGame) throws SQLException {
+		try (PreparedStatement preparedStatement = con.prepareStatement(INSERT_BOARD_GAME_QUERY,
+				Statement.RETURN_GENERATED_KEYS)) {
+			preparedStatement.setString(1, boardGame.getName());
+			preparedStatement.setInt(2, boardGame.getRentPrice());
+			preparedStatement.setInt(3, boardGame.getQuantity());
+
+			final int effectiveRow = preparedStatement.executeUpdate();
+
+			if (effectiveRow == 1) {
+				try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+					if (generatedKeys.next()) {
+						boardGame.setId(generatedKeys.getLong(ID_COLUMN));
+					}
+				}
+			}
+		}
+	}
+
+}
