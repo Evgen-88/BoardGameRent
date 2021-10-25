3904ae442fd1acc4cba8ba713c2f47a472f00436
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-10-25 08:57:23 +0300

    Add files via upload

--- src/main/resources/db/migration/V20211018120832__init.sql
+++ src/main/resources/db/migration/V20211018120832__init.sql
@@ -1,97 +1,100 @@
-CREATE TABLE IF NOT EXISTS 'users'
+CREATE TABLE IF NOT EXISTS users
 (
-	'id'		BIGINT			NOT NULL AUTO_INCREMENT,
-	'login'		VARCHAR(32)  	NOT NULL,
-	'password'	VARCHAR(256)	NOT NULL,
-	'name'		VARCHAR(32)  	NOT NULL,
-	'phone'		BIGINT 			NULL,
-	'email'		VARCHAR(32)  	NULL,
-	PRIMARY KEY ('id'),
-	CONSTRAINT UNIQUE_LOGIN UNIQUE ('login')
+	id			BIGINT			NOT NULL AUTO_INCREMENT,
+	login		VARCHAR(32)  	NOT NULL,
+	password	VARCHAR(256)	NOT NULL,
+	name		VARCHAR(32)  	NOT NULL,
+	phone		BIGINT 			NULL,
+	email		VARCHAR(32)  	NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT UNIQUE_LOGIN UNIQUE (login)
 );
 
-CREATE TABLE IF NOT EXISTS 'users_user_role_link' (
-	'role_id'	BIGINT	 NOT NULL,
-	'user_id'	BIGINT	 NOT NULL,
-	PRIMARY KEY ('role_id', 'user_id'),
-    CONSTRAINT 'users_user_role_link_users_fk'
-    	FOREIGN KEY ('user_id')
-        	REFERENCES 'users' ('id')
-            	ON DELETE NO ACTION
-            	ON UPDATE NO ACTION,
-    CONSTRAINT 'users_user_role_link_user_role_fk'
-        FOREIGN KEY ('role_id')
-            REFERENCES 'user_role' ('id')
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
+CREATE TABLE IF NOT EXISTS user_role
+(
+	id		BIGINT		NOT NULL AUTO_INCREMENT,
+	name	VARCHAR(32)	NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT UNIQUE_USER_NAME UNIQUE (name)
 );
 
-CREATE TABLE IF NOT EXISTS 'user_role' (
-	'id'		BIGINT		NOT NULL AUTO_INCREMENT,
-	'name'		VARCHAR(32)	NOT NULL,
-	PRIMARY KEY ('id'),
-	CONSTRAINT UNIQUE_NAME UNIQUE ('name')
+CREATE TABLE IF NOT EXISTS users_user_role_link
+(
+	role_id	BIGINT	 NOT NULL,
+	user_id	BIGINT	 NOT NULL,
+	PRIMARY KEY (role_id, user_id),
+    CONSTRAINT users_user_role_link_users_fk
+    	FOREIGN KEY (user_id)
+        	REFERENCES users (id),
+    CONSTRAINT users_user_role_link_user_role_fk
+        FOREIGN KEY (role_id)
+            REFERENCES user_role (id)
 );
 
-CREATE TABLE IF NOT EXISTS 'board_game' (
-	'id'			BIGINT			NOT NULL AUTO_INCREMENT,
-	'name'			VARCHAR(512)	NOT NULL,
-	'rent_price'	BIGINT 			NOT NULL,
-	'qoantity'		BIGINT 			NULL,
-	PRIMARY KEY ('id'),
-	CONSTRAINT UNIQUE_NAME UNIQUE ('name')
+CREATE TABLE IF NOT EXISTS orders
+(
+	id			BIGINT									NOT NULL AUTO_INCREMENT,
+	user_id		BIGINT									NOT NULL,
+	total_price	BIGINT 									NOT NULL,
+	order_date	DATE									NOT NULL,
+	status		ENUM('booked', 'rejected', 'confirmed')	NOT NULL,
+	PRIMARY KEY (id),
+    CONSTRAINT orders_users_fk
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
 );
 
-CREATE TABLE IF NOT EXISTS 'rent' (
-	'id'			BIGINT	NOT NULL AUTO_INCREMENT,
-	'board_game_id'	BIGINT	NOT NULL,
-	'order_id'		BIGINT	NOT NULL,
-	'from'			DATE	NOT NULL,
-	'to'			DATE	NOT NULL,
-	'price'			BIGINT 	NOT NULL,
-	PRIMARY KEY ('id'),
-    CONSTRAINT 'rent_board_game_fk'
-        FOREIGN KEY ('board_game_id')
-            REFERENCES 'board_game' ('id')
-    CONSTRAINT 'rent_orders_fk'
-        FOREIGN KEY ('order_id')
-            REFERENCES 'orders' ('id')
+CREATE TABLE IF NOT EXISTS board_game
+(
+	id			BIGINT			NOT NULL AUTO_INCREMENT,
+	name		VARCHAR(512)	NOT NULL,
+	rent_price	BIGINT 			NOT NULL,
+	quantity	BIGINT 			NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT UNIQUE_BOARD_GAME_NAME UNIQUE (name)
 );
 
-CREATE TABLE IF NOT EXISTS 'orders' (
-	'id'			BIGINT									NOT NULL AUTO_INCREMENT,
-	'user_id'		BIGINT									NOT NULL,
-	'total_price'	BIGINT 									NOT NULL,
-	'date'			DATE									NOT NULL,
-	'status'		ENUM('booked', 'rejected', 'confirmed')	NOT NULL,
-	PRIMARY KEY ('id'),
-    CONSTRAINT 'orders_users_fk'
-        FOREIGN KEY ('user_id')
-            REFERENCES 'users' ('id')
+CREATE TABLE IF NOT EXISTS rent
+(
+	id				BIGINT	NOT NULL AUTO_INCREMENT,
+	board_game_id	BIGINT	NOT NULL,
+	order_id		BIGINT	NOT NULL,
+	rent_from			DATE	NOT NULL,
+	rent_to				DATE	NOT NULL,
+	price			BIGINT 	NOT NULL,
+	PRIMARY KEY (id),
+    CONSTRAINT rent_board_game_fk
+        FOREIGN KEY (board_game_id)
+            REFERENCES board_game (id),
+    CONSTRAINT rent_orders_fk
+        FOREIGN KEY (order_id)
+            REFERENCES orders (id)
 );
 
-CREATE TABLE IF NOT EXISTS 'accessory' (
-	'id'		BIGINT			NOT NULL AUTO_INCREMENT,
-	'name'		VARCHAR(512) 	NOT NULL,
-	'price'		BIGINT 			NOT NULL,
-	'quantity'	BIGINT 			NULL,
-	PRIMARY KEY ('id'),
-	CONSTRAINT UNIQUE_NAME UNIQUE ('name')
+CREATE TABLE IF NOT EXISTS accessory
+(
+	id			BIGINT			NOT NULL AUTO_INCREMENT,
+	name		VARCHAR(512) 	NOT NULL,
+	price		BIGINT 			NOT NULL,
+	quantity	BIGINT 			NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT UNIQUE_ACCESSORY_NAME UNIQUE (name)
 );
 
-CREATE TABLE IF NOT EXISTS 'purchase' (
-	'id'			BIGINT	NOT NULL AUTO_INCREMENT,
-	'accessory_id'	BIGINT	NOT NULL,
-	'order_id'		BIGINT	NOT NULL,
-	'quantity'		BIGINT	NOT NULL,
-	'price'			BIGINT 	NOT NULL,
-	PRIMARY KEY ('id'),
-    CONSTRAINT 'purchase_accessory_fk'
-        FOREIGN KEY ('accessory_id')
-            REFERENCES 'accessory' ('id'),
-    CONSTRAINT 'purchase_orders_fk'
-        FOREIGN KEY ('order_id')
-            REFERENCES 'orders' ('id')
+CREATE TABLE IF NOT EXISTS purchase
+(
+	id				BIGINT	NOT NULL AUTO_INCREMENT,
+	accessory_id	BIGINT	NOT NULL,
+	order_id		BIGINT	NOT NULL,
+	quantity		BIGINT	NOT NULL,
+	price			BIGINT 	NOT NULL,
+	PRIMARY KEY (id),
+    CONSTRAINT purchase_accessory_fk
+        FOREIGN KEY (accessory_id)
+            REFERENCES accessory (id),
+    CONSTRAINT purchase_orders_fk
+        FOREIGN KEY (order_id)
+            REFERENCES orders (id)
 );
 
 COMMIT;
--- src/main/resources/db/migration/V20211024134106__fill.sql
+++ src/main/resources/db/migration/V20211024134106__fill.sql
@@ -1,48 +1,48 @@
-INSERT INTO users('id', 'login', 'password', 'name', 'phone', 'email')
-VALUES  (1, 'ivan', 'ivan', 'Иван', 1111111, 'ivan@mail'),
-	(2, 'alex', 'alex', 'Алекс', 2222222, 'alex@mail'),
-	(3, 'kir', 'kir', 'Кирилл', 3333333, 'kir@mail'),
-	(4, 'piter', 'piter', 'Петр', 4444444, 'piter@mail'),
-	(5, 'vova', 'vova', 'Владимир', 5555555, 'vova@mail');
+INSERT INTO users(login, password, name, phone, email)
+VALUES  ('ivan', 'ivan', 'Иван', 1111111, 'ivan@mail'),
+		('alex', 'alex', 'Алекс', 2222222, 'alex@mail'),
+		('kir', 'kir', 'Кирилл', 3333333, 'kir@mail'),
+		('piter', 'piter', 'Петр', 4444444, 'piter@mail'),
+		('vova', 'vova', 'Владимир', 5555555, 'vova@mail');
 		
-INSERT INTO users_user_role_link('role_id', 'user_id')
+INSERT INTO user_role(name)
+VALUES  ('admin'),
+		('manager'),
+		('customer');
+		
+INSERT INTO users_user_role_link(role_id, user_id)
 VALUES  (1, 1),
-	(2, 3),
-	(3, 1),
-	(3, 4),
-	(3, 2),
-	(3, 5);
+		(2, 3),
+		(3, 1),
+		(3, 4),
+		(3, 2),
+		(3, 5);
 		
-INSERT INTO user_role('id', 'name')
-VALUES  (1, 'admin'),
-	(2, 'manager'),
-	(3, 'customer');
+INSERT INTO orders(user_id, total_price, oorder_date, status)
+VALUES  (1, 45, '2021-10-23', 'confirmed'),
+		(4, 90, '2021-10-23', 'confirmed'),
+		(2, 126, '2021-10-23', 'confirmed'),
+		(5, 120, '2021-10-25', 'booked');
 		
-INSERT INTO board_game('id', 'name', 'rent_price', 'quantity')
-VALUES  (1, 'Сквозь века', 45, 3),
-	(2, 'Сумеречная борьба', 40, 2),
-	(3, 'Звездные Войны. Восстание', 50, 2),
-	(4, 'Особняки безумия', 50, 4);
+INSERT INTO board_game(name, rent_price, quantity)
+VALUES  ('Сквозь века', 45, 3),
+		('Сумеречная борьба', 40, 2),
+		('Звездные Войны. Восстание', 50, 2),
+		('Особняки безумия', 50, 4);
 
-INSERT INTO rent('id', 'board_game_id', 'order_id', 'from', 'to', 'price')
-VALUES  (1, 1, 1, '2021-10-23', '2021-10-24', 45),
-	(2, 1, 2, '2021-10-23', '2021-10-25', 90),
-	(3, 3, 3, '2021-10-22', '2021-10-24', 100),
-	(4, 2, 4, '2021-10-25', '2021-10-28', 120);
-
-INSERT INTO orders('id', 'user_id', 'total_price', 'date', 'status')
-VALUES  (1, 1, 45, '2021-10-23', 'confirmed'),
-	(2, 4, 90, '2021-10-23', 'confirmed'),
-	(3, 2, 126, '2021-10-23', 'confirmed'),
-	(4, 5, 120, '2021-10-25', 'booked');
+INSERT INTO rent(board_game_id, order_id, rent_from, rent_to, price)
+VALUES  (1, 1, '2021-10-23', '2021-10-24', 45),
+		(1, 2, '2021-10-23', '2021-10-25', 90),
+		(3, 3, '2021-10-22', '2021-10-24', 100),
+		(2, 4, '2021-10-25', '2021-10-28', 120);
 		
-INSERT INTO accessory('id', 'name', 'price', 'quantity')
-VALUES  (1, 'Протекторы для карт 65х87', 12, 24),
-	(2, 'Протекторы для карт 48х64', 13, 20),
-	(3, 'Протекторы для карт 102х143', 18, 11);
+INSERT INTO accessory(name, price, quantity)
+VALUES  ('Протекторы для карт 65х87', 12, 24),
+		('Протекторы для карт 48х64', 13, 20),
+		('Протекторы для карт 102х143', 18, 11);
 		
-INSERT INTO purchase('id', 'accessory_id', 'order_id', 'quantity', 'price')
-VALUES  (1, 2, 3, 2, 26);
+INSERT INTO purchase(accessory_id, order_id, quantity, price)
+VALUES  (2, 3, 2, 26);
 
 COMMIT;
 
