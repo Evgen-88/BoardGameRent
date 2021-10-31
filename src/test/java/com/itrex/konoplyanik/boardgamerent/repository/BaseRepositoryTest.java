e0ad1ee880a7caf8f91c448f97c903802f633753
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-10-31 13:10:32 +0300

    Add files via upload

--- src/test/java/com/itrex/konoplyanik/boardgamerent/repository/BaseRepositoryTest.java
+++ src/test/java/com/itrex/konoplyanik/boardgamerent/repository/BaseRepositoryTest.java
@@ -1,21 +1,25 @@
 package com.itrex.konoplyanik.boardgamerent.repository;
 
 import org.h2.jdbcx.JdbcConnectionPool;
+import org.hibernate.SessionFactory;
 import org.junit.After;
 import org.junit.Before;
 
 import static com.itrex.konoplyanik.boardgamerent.properties.Properties.*;
 
 import com.itrex.konoplyanik.boardgamerent.service.FlywayService;
+import com.itrex.konoplyanik.boardgamerent.util.HibernateUtil;
 
 public abstract class BaseRepositoryTest {
 	
 	private final FlywayService flywayService;
 	private final JdbcConnectionPool connectionPool;
+	private final SessionFactory sessionfactory;
 	
 	public BaseRepositoryTest() {
 		flywayService = new FlywayService();
 		connectionPool = JdbcConnectionPool.create(H2_URL, H2_USER, H2_PASSWORD);
+		sessionfactory = HibernateUtil.getSessionFactory();
 	}
 	
 	@Before
@@ -30,6 +34,10 @@ public abstract class BaseRepositoryTest {
 
     public JdbcConnectionPool getConnectionPool() {
         return connectionPool;
+    }
+    
+    public SessionFactory getSessionFactory() {
+        return sessionfactory;
     }
 	
 }
