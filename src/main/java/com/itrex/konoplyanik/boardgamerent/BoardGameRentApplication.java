1254feb9aaec84fae3f3232b0e0fc9fd2320687e
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-03 00:19:47 +0300

    spring framework was plugged in

--- src/main/java/com/itrex/konoplyanik/boardgamerent/BoardGameRentApplication.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/BoardGameRentApplication.java
@@ -1,13 +1,19 @@
 package com.itrex.konoplyanik.boardgamerent;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.itrex.konoplyanik.boardgamerent.config.ApplicationContextConfiguration;
 import com.itrex.konoplyanik.boardgamerent.service.FlywayService;
 
 public class BoardGameRentApplication {
 	
 	public static void main(String[] args) {
-		FlywayService flywayService = new FlywayService();
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationContextConfiguration.class);
+		FlywayService flywayService = applicationContext.getBean(FlywayService.class);
 		flywayService.migrate();
-
+		
+		flywayService.clean();
 	}
 	
 }
