c49c696b6bf5f2bb5d4477615a8d9c7a7a6ad6ea
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-03 21:26:08 +0300

    Flyway and SessionFactory Beans were configured

--- src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
@@ -1,31 +1,34 @@
 package com.itrex.konoplyanik.boardgamerent.config;
 
-import org.hibernate.Session;
+import static com.itrex.konoplyanik.boardgamerent.properties.Properties.H2_PASSWORD;
+import static com.itrex.konoplyanik.boardgamerent.properties.Properties.H2_URL;
+import static com.itrex.konoplyanik.boardgamerent.properties.Properties.H2_USER;
+import static com.itrex.konoplyanik.boardgamerent.properties.Properties.MIGRATION_LOCATION;
+
+import org.flywaydb.core.Flyway;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-import com.itrex.konoplyanik.boardgamerent.service.FlywayService;
-import com.itrex.konoplyanik.boardgamerent.util.HibernateUtil;
-
 @Configuration
 @ComponentScan("com.itrex.konoplyanik.boardgamerent")
 public class ApplicationContextConfiguration {
 
 	@Bean
-	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-	public Session session() {
-		return HibernateUtil.getSessionFactory().openSession();
+	public SessionFactory sessionFactory() {
+		return new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
 	}
 	
+
 	@Bean
 	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-	public FlywayService flywayService() {
-		FlywayService flywayService = new FlywayService();
-		flywayService.migrate();
-		return flywayService;
+	public Flyway flyway() {
+		Flyway flyway = Flyway.configure().dataSource(H2_URL, H2_USER, H2_PASSWORD).locations(MIGRATION_LOCATION)
+				.load();
+		return flyway;
 	}
-	
+
 }
