5a7541aa071ed23b788902a120479d91c8b0479a
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-04 00:08:55 +0300

    spring-test plugged in

--- src/test/java/com/itrex/konoplyanik/boardgamerent/repository/BaseRepositoryTest.java
+++ src/test/java/com/itrex/konoplyanik/boardgamerent/repository/BaseRepositoryTest.java
@@ -1,36 +1,30 @@
 package com.itrex.konoplyanik.boardgamerent.repository;
 
+import org.flywaydb.core.Flyway;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import com.itrex.konoplyanik.boardgamerent.config.ApplicationContextConfiguration;
-import com.itrex.konoplyanik.boardgamerent.service.FlywayService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.itrex.konoplyanik.boardgamerent.config.ApplicationContextConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ApplicationContextConfiguration.class)
 public abstract class BaseRepositoryTest {
 	
-	private final ApplicationContext applicationContext;
-	private final FlywayService flywayService;
-	
-	public BaseRepositoryTest() {
-		applicationContext = new AnnotationConfigApplicationContext(ApplicationContextConfiguration.class);
-		flywayService = applicationContext.getBean(FlywayService.class);
-	}
+	@Autowired
+	private Flyway flyway;
 	
 	@Before
     public void initDB() {
-        flywayService.migrate();
+        flyway.migrate();
     }
 
-    @After
+	@After
     public void cleanDB() {
-        flywayService.clean();
-    }
-
-    @Autowired
-    public ApplicationContext getApplicationContext() {
-        return applicationContext;
+        flyway.clean();
     }
 
 }
