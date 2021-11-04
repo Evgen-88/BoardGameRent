23925cb89875585ee70d520c9dead8bca3900725
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-04 09:10:46 +0300

    links on unexisted class were removed

--- src/main/java/com/itrex/konoplyanik/boardgamerent/BoardGameRentApplication.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/BoardGameRentApplication.java
@@ -2,18 +2,18 @@ package com.itrex.konoplyanik.boardgamerent;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.flywaydb.core.Flyway;
 
 import com.itrex.konoplyanik.boardgamerent.config.ApplicationContextConfiguration;
-import com.itrex.konoplyanik.boardgamerent.service.FlywayService;
 
 public class BoardGameRentApplication {
 	
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationContextConfiguration.class);
-		FlywayService flywayService = applicationContext.getBean(FlywayService.class);
-		flywayService.migrate();
+		Flyway flyway = applicationContext.getBean(Flyway.class);
+		flyway.migrate();
 		
-		flywayService.clean();
+		flyway.clean();
 	}
 	
 }
