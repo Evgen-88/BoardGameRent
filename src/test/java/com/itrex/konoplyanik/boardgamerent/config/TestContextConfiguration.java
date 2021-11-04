2981205d83cad14a5cc7ac35d93609bd1a8151f3
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-04 17:41:36 +0300

    TestContextConfiguration & TestProperties added

---
+++ src/test/java/com/itrex/konoplyanik/boardgamerent/config/TestContextConfiguration.java
@@ -0,0 +1,33 @@
+package com.itrex.konoplyanik.boardgamerent.config;
+
+import static com.itrex.konoplyanik.boardgamerent.properties.TestProperties.H2_PASSWORD;
+import static com.itrex.konoplyanik.boardgamerent.properties.TestProperties.H2_URL;
+import static com.itrex.konoplyanik.boardgamerent.properties.TestProperties.H2_USER;
+import static com.itrex.konoplyanik.boardgamerent.properties.TestProperties.MIGRATION_LOCATION;
+
+import org.flywaydb.core.Flyway;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@ComponentScan("com.itrex.konoplyanik.boardgamerent.repository")
+public class TestContextConfiguration {
+
+	@Bean
+	public SessionFactory sessionFactory() {
+		return new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
+	}
+	
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public Flyway flyway() {
+		Flyway flyway = Flyway.configure().dataSource(H2_URL, H2_USER, H2_PASSWORD).locations(MIGRATION_LOCATION)
+				.load();
+		return flyway;
+	}
+	
+}
---
+++ src/test/java/com/itrex/konoplyanik/boardgamerent/properties/TestProperties.java
@@ -0,0 +1,10 @@
+package com.itrex.konoplyanik.boardgamerent.properties;
+
+public class TestProperties {
+
+	public static final String MIGRATION_LOCATION = "db/migration";
+	public static final String H2_URL = "jdbc:h2:mem:boardgamerent;DB_CLOSE_DELAY=-1";
+	public static final String H2_USER = "evgen";
+	public static final String H2_PASSWORD = "evgen";
+	
+}
