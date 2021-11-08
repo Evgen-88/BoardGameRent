a56f12a8aadf389ccfe5a3fb8bd4512ac541fbbe
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-09 00:39:19 +0300

    Some fixes

--- src/main/java/com/itrex/konoplyanik/boardgamerent/service/BoardGameService.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/service/BoardGameService.java
@@ -3,14 +3,12 @@ package com.itrex.konoplyanik.boardgamerent.service;
 import java.util.List;
 
 import com.itrex.konoplyanik.boardgamerent.dto.BoardGameDTO;
-import com.itrex.konoplyanik.boardgamerent.dto.BoardGameListDTO;
 import com.itrex.konoplyanik.boardgamerent.exception.ServiceException;
 
 public interface BoardGameService {
 
-	List<BoardGameListDTO> findAll() throws ServiceException;
+	List<BoardGameDTO> findAll() throws ServiceException;
 	BoardGameDTO findById(Long id) throws ServiceException;
-	BoardGameListDTO findListById(Long id) throws ServiceException;
 	BoardGameDTO add(BoardGameDTO boardGame) throws ServiceException;
 	BoardGameDTO update(BoardGameDTO boardGame) throws ServiceException;
 	boolean delete(Long id) throws ServiceException;
