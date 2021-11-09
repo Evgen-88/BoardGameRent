5e6a540fca805bdf7022621520add82f0db98e38
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-09 22:06:00 +0300

    @ComponentScan fixed

--- src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
@@ -7,14 +7,12 @@ import static com.itrex.konoplyanik.boardgamerent.properties.Properties.MIGRATIO
 
 import org.flywaydb.core.Flyway;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan("com.itrex.konoplyanik.boardgamerent.repository")
+@ComponentScan("com.itrex.konoplyanik.boardgamerent")
 public class ApplicationContextConfiguration {
 
 	@Bean
