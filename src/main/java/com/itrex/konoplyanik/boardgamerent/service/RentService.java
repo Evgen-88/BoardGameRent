620b221e5fc4131aeca5eec24d2494e497bd951a
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-09 22:23:00 +0300

    Service fixes

--- src/main/java/com/itrex/konoplyanik/boardgamerent/service/RentService.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/service/RentService.java
@@ -1,16 +1,13 @@
 package com.itrex.konoplyanik.boardgamerent.service;
 
-import java.util.List;
-
 import com.itrex.konoplyanik.boardgamerent.dto.RentDTO;
+import com.itrex.konoplyanik.boardgamerent.dto.RentSaveDTO;
 import com.itrex.konoplyanik.boardgamerent.exception.ServiceException;
 
 public interface RentService {
 
 	RentDTO findById(Long id) throws ServiceException;
-	RentDTO add(RentDTO rent) throws ServiceException;
-	RentDTO update(RentDTO rent) throws ServiceException;
+	RentDTO add(RentSaveDTO rent) throws ServiceException;
+	RentDTO update(RentSaveDTO rent) throws ServiceException;
 	boolean delete(Long id) throws ServiceException;
-	
-	List<RentDTO> findRentsByOrder(Long orderId) throws ServiceException;
 }
