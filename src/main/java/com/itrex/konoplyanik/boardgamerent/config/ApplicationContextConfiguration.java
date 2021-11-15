d7bc78bce0891e49463e6c23b0a46d25de8f169f
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-15 09:47:54 +0300

    Add Advices

--- src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
@@ -10,8 +10,10 @@ import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan("com.itrex.konoplyanik.boardgamerent")
 public class ApplicationContextConfiguration {
 
