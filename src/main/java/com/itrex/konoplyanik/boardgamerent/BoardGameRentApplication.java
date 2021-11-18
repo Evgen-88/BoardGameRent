537906e45afc61abdf487b4c9d893ddfb32c5239
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-18 11:00:13 +0300

    Add Profiles

--- src/main/java/com/itrex/konoplyanik/boardgamerent/BoardGameRentApplication.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/BoardGameRentApplication.java
@@ -2,16 +2,25 @@ package com.itrex.konoplyanik.boardgamerent;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.AbstractEnvironment;
 
 import com.itrex.konoplyanik.boardgamerent.config.ApplicationContextConfiguration;
 import com.itrex.konoplyanik.boardgamerent.repository.RoleRepository;
 
-public class BoardGameRentApplication {
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+public class BoardGameRentApplication {
+	
 	public static void main(String[] args) {
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "info");
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationContextConfiguration.class);
+		
 		RoleRepository roleRepository = applicationContext.getBean(RoleRepository.class);
-		System.out.print(roleRepository.findById(1L));
+		System.out.print(roleRepository.delete(2L));
+		
+		log.info("info");
+		log.debug("debug");
 	}
 
 }
