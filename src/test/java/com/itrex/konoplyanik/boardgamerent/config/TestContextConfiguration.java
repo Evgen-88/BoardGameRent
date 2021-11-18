28e618a0e98102e4b6a4a1698ca69d739129bc2a
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-18 11:02:49 +0300

    Add Properties file

--- src/test/java/com/itrex/konoplyanik/boardgamerent/config/TestContextConfiguration.java
+++ src/test/java/com/itrex/konoplyanik/boardgamerent/config/TestContextConfiguration.java
@@ -1,20 +1,31 @@
 package com.itrex.konoplyanik.boardgamerent.config;
 
-import static com.itrex.konoplyanik.boardgamerent.properties.TestProperties.H2_PASSWORD;
-import static com.itrex.konoplyanik.boardgamerent.properties.TestProperties.H2_URL;
-import static com.itrex.konoplyanik.boardgamerent.properties.TestProperties.H2_USER;
-import static com.itrex.konoplyanik.boardgamerent.properties.TestProperties.MIGRATION_LOCATION;
-
 import org.flywaydb.core.Flyway;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:/application.properties")
 @ComponentScan("com.itrex.konoplyanik.boardgamerent")
 public class TestContextConfiguration {
 
+	@Value("${database.url}")
+	private String url;
+	@Value("${database.user}")
+	private String user;
+	@Value("${database.password}")
+	private String password;
+	@Value("${database.migration.location}")
+	private String migrationLocation;
+	@Value("${logging.profile.info}")
+	private String loggingProfileInfo;
+	@Value("${logging.profile.debug}")
+	private String loggingProfileDebug;
+	
 	@Bean
 	public SessionFactory sessionFactory() {
 		return new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
@@ -22,9 +33,10 @@ public class TestContextConfiguration {
 	
 	@Bean(initMethod = "migrate")
 	public Flyway flyway() {
-		Flyway flyway = Flyway.configure().dataSource(H2_URL, H2_USER, H2_PASSWORD).locations(MIGRATION_LOCATION)
+		return Flyway.configure()
+				.dataSource(url, user, password)
+				.locations(migrationLocation)
 				.load();
-		return flyway;
 	}
 	
 }
