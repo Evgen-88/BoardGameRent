821c09c019fd4d0b12b90907e502eebf65ee6483
Konoplyanik Evgeniy <kei1988@mail.ru>
2021-11-19 22:58:12 +0300

    Change hibernate xml on properties

--- src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
@@ -1,8 +1,11 @@
 package com.itrex.konoplyanik.boardgamerent.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
 import org.apache.log4j.PropertyConfigurator;
 import org.flywaydb.core.Flyway;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,30 +33,64 @@ public class ApplicationContextConfiguration {
 	private String password;
 	@Value("${database.migration.location}")
 	private String migrationLocation;
+	@Value("${hibernate.driver}")
+	private String hibernateDriver;
+	@Value("${hibernate.dialect}")
+	private String hibernateDialect;
+	@Value("${hibernate.show_sql}")
+	private String hibernateShowSql;
+	@Value("${hibernate.format_sql}")
+	private String hibernateFormatSql;
+	@Value("${entity.path}")
+	private String entityPath;
 	@Value("${logging.profile.info}")
 	private String loggingProfileInfo;
 	@Value("${logging.profile.debug}")
 	private String loggingProfileDebug;
-	
+
 	@Bean
-	public SessionFactory sessionFactory() {
-		return new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
-	}
-	
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(entityPath);
+        sessionFactory.setHibernateProperties(properties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(hibernateDriver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public Properties properties() {
+        Properties properties = new Properties();
+        properties.setProperty("connection.url", url);
+        properties.setProperty("connection.driver_class", hibernateDriver);
+        properties.setProperty("connection.username", user);
+        properties.setProperty("connection.password", password);
+        properties.setProperty("dialect", hibernateDialect);
+        properties.setProperty("show_sql", hibernateShowSql);
+        properties.setProperty("format_sql", hibernateFormatSql);
+        return properties;
+    }
+
 	@Bean(initMethod = "migrate")
 	public Flyway flyway() {
-		return Flyway.configure()
-				.dataSource(url, user, password)
-				.locations(migrationLocation)
-				.load();
+		return Flyway.configure().dataSource(url, user, password).locations(migrationLocation).load();
 	}
-	
+
 	@Bean
 	@Profile("info")
 	public void configureLoggingInfo() {
 		PropertyConfigurator.configure(loggingProfileInfo);
 	}
-	
+
 	@Bean
 	@Profile("debug")
 	public void configureLoggingDebug() {
