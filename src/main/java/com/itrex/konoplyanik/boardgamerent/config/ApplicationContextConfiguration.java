d92185ba385e7d733688ac90e9a177e40238fa15
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-22 18:16:40 +0300

    Change log-property paths

--- src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
@@ -1,30 +1,23 @@
 package com.itrex.konoplyanik.boardgamerent.config;
 
 import org.apache.log4j.PropertyConfigurator;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
-@PropertySource("classpath:/application.properties")
 public class ApplicationContextConfiguration {
-	@Value("${logging.profile.info}")
-	private String loggingProfileInfo;
-	@Value("${logging.profile.debug}")
-	private String loggingProfileDebug;
 
 	@Bean
 	@Profile("info")
 	public void configureLoggingInfo() {
-		PropertyConfigurator.configure(loggingProfileInfo);
+		PropertyConfigurator.configure("src/main/resources/log4j_info.properties");
 	}
 
 	@Bean
 	@Profile("debug")
 	public void configureLoggingDebug() {
-		PropertyConfigurator.configure(loggingProfileDebug);
+		PropertyConfigurator.configure("src/main/resources/log4j_debug.properties");
 	}
 }
