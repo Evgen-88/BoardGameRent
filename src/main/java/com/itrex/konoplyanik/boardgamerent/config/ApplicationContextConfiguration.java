d0ae9c05a7745681813c56e23b15f2328f7e31bd
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-11-23 21:58:59 +0300

    Refactoring application config

--- src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/config/ApplicationContextConfiguration.java
@@ -1,6 +1,7 @@
 package com.itrex.konoplyanik.boardgamerent.config;
 
 import org.apache.log4j.PropertyConfigurator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,16 +9,20 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationContextConfiguration {
+	@Value("${logging.profile.info}")
+	private String loggingProfileInfo;
+	@Value("${logging.profile.debug}")
+	private String loggingProfileDebug;
 
 	@Bean
 	@Profile("info")
 	public void configureLoggingInfo() {
-		PropertyConfigurator.configure("src/main/resources/log4j_info.properties");
+		PropertyConfigurator.configure(loggingProfileInfo);
 	}
 
 	@Bean
 	@Profile("debug")
 	public void configureLoggingDebug() {
-		PropertyConfigurator.configure("src/main/resources/log4j_debug.properties");
+		PropertyConfigurator.configure(loggingProfileDebug);
 	}
 }
