2739a544389e6aa0d027a0a296624b93c48a9619
Evgeniy Konoplianik <evgeniy.kanaplianik@itrexgroup.com>
2022-01-12 11:24:41 +0300

    Removed package imports with single imports

--- src/main/java/com/itrex/konoplyanik/boardgamerent/controller/OrderController.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/controller/OrderController.java
@@ -1,12 +1,23 @@
 package com.itrex.konoplyanik.boardgamerent.controller;
 
-import com.itrex.konoplyanik.boardgamerent.dto.*;
+import com.itrex.konoplyanik.boardgamerent.dto.OrderDTO;
+import com.itrex.konoplyanik.boardgamerent.dto.OrderListDTO;
+import com.itrex.konoplyanik.boardgamerent.dto.OrderSaveDTO;
+import com.itrex.konoplyanik.boardgamerent.dto.PurchaseDTO;
+import com.itrex.konoplyanik.boardgamerent.dto.RentDTO;
 import com.itrex.konoplyanik.boardgamerent.service.OrderService;
 import com.itrex.konoplyanik.boardgamerent.service.PurchaseService;
 import com.itrex.konoplyanik.boardgamerent.service.RentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
