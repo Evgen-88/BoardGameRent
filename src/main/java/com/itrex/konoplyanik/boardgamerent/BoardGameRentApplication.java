64dc9d484d1610557c62a31f3324f9e57afe2c0a
Evgeniy Konoplianik <evgeniy.kanaplianik@itrexgroup.com>
2022-02-02 00:24:53 +0300

    Add property encryption

--- src/main/java/com/itrex/konoplyanik/boardgamerent/BoardGameRentApplication.java
+++ src/main/java/com/itrex/konoplyanik/boardgamerent/BoardGameRentApplication.java
@@ -1,15 +1,10 @@
 package com.itrex.konoplyanik.boardgamerent;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BoardGameRentApplication {
-
-	@Autowired
-	ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardGameRentApplication.class, args);
